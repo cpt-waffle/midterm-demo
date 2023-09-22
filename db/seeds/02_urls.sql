b7bb2db1d6b9f65ee8286b9b428d85cf819a58ff
vasiliy-klimkin <vasily.klimkin@gmail.com>
2023-09-22 12:09:05 -0400

    add urls and seeds

---
+++ db/seeds/02_urls.sql
@@ -0,0 +1,5 @@
+INSERT INTO urls (long_url, short_url) VALUES ('www.google.ca', 'S95fs4');
+INSERT INTO urls (long_url, short_url) VALUES ('www.reddit.com', '42qf5f');
+INSERT INTO urls (long_url, short_url) VALUES ('www.old.reddit.com', '352f5f');
+INSERT INTO urls (long_url, short_url) VALUES ('www.facebook.com', 'ZZ5fa41');
+INSERT INTO urls (long_url, short_url) VALUES ('www.yahoo.ca', 'OkmF3fg');
